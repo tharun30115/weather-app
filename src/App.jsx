7ae5330b8c38c8414808eab230f9fe6eba3408db
Tharun <tharungowda30115@gmail.com>
2025-02-18 22:08:35 +0530

    msg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
 const App = () => {
   return (
-    <div className='app'>
-      
+    <div className="container">
+      <div className="search-section">
+        <form action="#" className="search-form">
+          <span className="material-symbols-rounded">search</span>
+          <input
+            type="search"
+            placeholder="Enter a city name"
+            className="search-input"
+            required
+          />
+        </form>
+        <button className="location-button">
+          <span class="material-symbols-outlined">arrows_input</span>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
